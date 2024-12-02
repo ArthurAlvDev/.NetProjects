341d9bc7f177ca4e1c145bdb6111e03dd86b369f
ArthurAlvDev <69433870+ArthurAlvDev@users.noreply.github.com>
2024-12-01 23:53:05 -0300

    Pronto
    Seguindo as indicações pedidas para o projeto

---
+++ Dio/Formação .NET Developer/trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DesafioPOO.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Iniciando o programa...");
+
+        // Criando um smartphone Nokia
+        Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia 3310", imei: "1111111111", memoria: 32);
+        Console.WriteLine("Teste com Nokia:");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // Criando um smartphone iPhone
+        Smartphone iphone = new Iphone(numero: "987654", modelo: "iPhone 13", imei: "2222222222", memoria: 128);
+        Console.WriteLine("Teste com iPhone:");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
